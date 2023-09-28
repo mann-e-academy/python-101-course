1dd4f3d272499d9da3c09e0c03186c355501e0f8
Muhammadreza Haghiri <haghiri75@gmail.com>
2023-09-29 02:34:55 +0330

    Variables in Python

---
+++ ep03.py
@@ -0,0 +1,64 @@
+print("Variables in python:")
+
+## Integer 
+
+a = 2 
+b = 4
+
+print("a =", a)
+print("b =", b)
+
+### Arithmetic operator
+
+c = a + b
+
+print("a + b =", c)
+
+c = b - a 
+print("b - a =", c)
+
+c = a * b 
+print("a * b =", c)
+
+c = b / a
+print("b / a =", c)
+
+c = a ** b 
+print("a ** b =", c)
+
+c = b % a 
+print("b % a =", c)
+
+### Floating Point 
+
+a = 2.5
+b = 3
+
+print("Type of a:", type(a))
+print("Type of b", type(b))
+
+c = a + b
+
+print("a + b =", c)
+
+# Boolean
+
+a = True 
+b = False 
+
+print("Type of a:", type(a))
+print("Type of b", type(b))
+
+# Logical Operator
+
+c = a and b 
+print("a && b =", c)
+
+c = a or b 
+print("a | b =", c)
+
+c = not a 
+print("!a =", c) 
+
+c = not b 
+print("!b =", c)
